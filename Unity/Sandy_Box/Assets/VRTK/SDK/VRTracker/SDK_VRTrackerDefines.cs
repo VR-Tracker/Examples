783f6a153f4f43783f31125e07dc630bc5c0a529
Jules Thuillier <thuillierjules@gmail.com>
2018-01-05 02:24:53 -0500

    VRTK import and first SDK integration

---
+++ Unity/Sandy_Box/Assets/VRTK/SDK/VRTracker/SDK_VRTrackerDefines.cs
@@ -0,0 +1,28 @@
+﻿// VRTracker Defines|SDK_VRTracker|001
+namespace VRTK
+{
+    using System;
+    using System.Reflection;
+
+    /// <summary>
+    /// Handles all the scripting define symbols for the Oculus and Avatar SDKs.
+    /// </summary>
+    public static class SDK_VRTrackerDefines
+    {
+        /// <summary>
+        /// The scripting define symbol for the Oculus SDK.
+        /// </summary>
+        public const string ScriptingDefineSymbol = SDK_ScriptingDefineSymbolPredicateAttribute.RemovableSymbolPrefix + "SDK_VRTRACKER";
+        /// <summary>
+        /// The scripting define symbol for the Oculus Avatar SDK.
+        /// </summary>
+        public const string AvatarScriptingDefineSymbol = SDK_ScriptingDefineSymbolPredicateAttribute.RemovableSymbolPrefix + "SDK_VRTRACKER_AVATAR";
+
+		[SDK_ScriptingDefineSymbolPredicate(ScriptingDefineSymbol, "Standalone")]
+		[SDK_ScriptingDefineSymbolPredicate(ScriptingDefineSymbol, "Android")]
+		private static bool IsVRTrackerAvailable()
+		{
+			return true;//VRTK_SharedMethods.GetTypeUnknownAssembly("Ximmerse.InputSystem.XDevicePlugin") != null; //TODO: change to check WS state
+		}
+    }
+}
