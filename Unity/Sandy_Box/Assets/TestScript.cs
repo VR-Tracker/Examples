bce75e44f30229da70595fc9fcf2611b9f03bd0b
Unknown <thuillierjules@gmail.com>
2018-01-09 18:35:40 -0500

    Auto stash before merge of "master" and "origin/master"
    merge to test

---
+++ Unity/Sandy_Box/Assets/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI() {
+		if (Network.isServer)
+			GUI.Label (new Rect (10, 10, 100, 20), "Running as a server");
+		else
+			if (Network.isClient)
+				GUI.Label (new Rect (10, 10, 100, 20), "Running as a client");
+
+	}
+}
