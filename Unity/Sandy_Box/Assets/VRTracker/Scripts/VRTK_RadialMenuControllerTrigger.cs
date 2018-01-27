ad920e4a3c1bf6b771ee24b193eb241db4c93b17
Jules Thuillier <thuillierjules@gmail.com>
2018-01-27 15:52:32 +0800

    New radial menu controller to use button as no click on trapckpad

---
+++ Unity/Sandy_Box/Assets/VRTracker/Scripts/VRTK_RadialMenuControllerTrigger.cs
@@ -0,0 +1,81 @@
+﻿namespace VRTK
+{
+	using UnityEngine;
+	[RequireComponent(typeof(VRTK_RadialMenu))]
+	public class VRTK_RadialMenuControllerTrigger : VRTK_RadialMenuController {
+
+		protected override void OnEnable()
+		{
+			if (events == null)
+			{
+				VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_NOT_INJECTED, "RadialMenuController", "VRTK_ControllerEvents", "events", "the parent"));
+				return;
+			}
+			else
+			{
+				events.TouchpadPressed += new ControllerInteractionEventHandler(DoTouchpadClicked);
+				events.TouchpadReleased += new ControllerInteractionEventHandler(DoTouchpadUnclicked);
+				events.TouchpadTouchStart += new ControllerInteractionEventHandler(DoTouchpadTouched);
+				events.TouchpadTouchEnd += new ControllerInteractionEventHandler(DoTouchpadUntouched);
+				events.TouchpadAxisChanged += new ControllerInteractionEventHandler(DoTouchpadAxisChanged);
+				events.TriggerPressed += new ControllerInteractionEventHandler(DoTriggerClicked);
+				events.TriggerReleased += new ControllerInteractionEventHandler(DoTriggerUnclicked);
+
+				menu.FireHapticPulse += new HapticPulseEventHandler(AttemptHapticPulse);
+			}
+		}
+
+		protected override void OnDisable()
+		{
+			events.TouchpadPressed -= new ControllerInteractionEventHandler(DoTouchpadClicked);
+			events.TouchpadReleased -= new ControllerInteractionEventHandler(DoTouchpadUnclicked);
+			events.TouchpadTouchStart -= new ControllerInteractionEventHandler(DoTouchpadTouched);
+			events.TouchpadTouchEnd -= new ControllerInteractionEventHandler(DoTouchpadUntouched);
+			events.TouchpadAxisChanged -= new ControllerInteractionEventHandler(DoTouchpadAxisChanged);
+			events.TriggerPressed -= new ControllerInteractionEventHandler(DoTriggerClicked);
+			events.TriggerReleased -= new ControllerInteractionEventHandler(DoTriggerUnclicked);
+
+			menu.FireHapticPulse -= new HapticPulseEventHandler(AttemptHapticPulse);
+		}
+
+		protected virtual void DoTouchpadTouched(object sender, ControllerInteractionEventArgs e)
+		{
+			if (!menu.isShown)
+				return;
+			touchpadTouched = true;
+			DoClickButton();
+		}
+
+		protected virtual void DoTouchpadUntouched(object sender, ControllerInteractionEventArgs e)
+		{
+			if (!menu.isShown)
+				return;
+			touchpadTouched = false;
+			DoUnClickButton();
+		}
+
+		protected virtual void DoTouchpadClicked(object sender, ControllerInteractionEventArgs e)
+		{
+			
+		}
+
+		protected virtual void DoTouchpadUnclicked(object sender, ControllerInteractionEventArgs e)
+		{
+			
+		}
+
+		protected virtual void DoTriggerClicked(object sender, ControllerInteractionEventArgs e)
+		{
+			if(menu.isShown)
+				DoHideMenu(false);
+			else
+				DoShowMenu(CalculateAngle(e));
+		}
+
+		protected virtual void DoTriggerUnclicked(object sender, ControllerInteractionEventArgs e)
+		{
+			
+		}
+	}
+
+}
